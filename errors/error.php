6afaaaf20bf23c5c1c48ea2012f43f73998ee0ce
alexxstevens <41077253+alexxstevens@users.noreply.github.com>
2020-05-06 20:44:35 -0500

    updated gitignore

--- errors/error.php
+++ errors/error.php
@@ -1,7 +1,6 @@
-<?php include '../view/header.php'; ?>
+
     
     <main>
         <h2 class="top">Error</h2>
         <p><?php echo $error; ?></p>
     </main>
-<?php include '../view/footer.php'; ?>
